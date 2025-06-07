6944050c962c3a26883716f0eea29a0c861ece93
TuPM <tupm.citgroup@gmail.com>
2025-06-07 22:58:41 +0700

    add

--- ros2_ws/xe_tham_do/launch/joystick.launch.py
+++ ros2_ws/xe_tham_do/launch/joystick.launch.py
@@ -1,29 +1,20 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    joy_params = os.path.join(get_package_share_directory('xe_tham_do'),'config','joystick.yaml')
+    joy_params = os.path.join(get_package_share_directory('xe_tham_do'), 'config', 'joystick.yaml')
 
-    joy_node = Node(
-            package='joy',
-            executable='joy_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-         )
+    joy_node = Node(package='joy', executable='joy_node', parameters=[joy_params, {'use_sim_time': use_sim_time}], )
 
-    teleop_node = Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel','/cmd_vel_joy')]
-         )
+    teleop_node = Node(package='teleop_twist_joy', executable='teleop_node', name='teleop_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}], remappings=[('/cmd_vel', '/cmd_vel_joy')])
 
     # twist_stamper = Node(
     #         package='twist_stamper',
@@ -33,13 +24,7 @@ def generate_launch_description():
     #                     ('/cmd_vel_out','/diff_cont/cmd_vel')]
     #      )
 
-
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-        joy_node,
-        teleop_node,
-        # twist_stamper       
-    ])
+    return LaunchDescription(
+        [DeclareLaunchArgument('use_sim_time', default_value='false', description='Use sim time if true'), joy_node,
+            teleop_node, # twist_stamper
+        ])
--- ros2_ws/xe_tham_do/launch/localization_launch.py
+++ ros2_ws/xe_tham_do/launch/localization_launch.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2018 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -40,68 +26,36 @@ def generate_launch_description():
     # https://github.com/ros/robot_state_publisher/pull/30
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'yaml_filename': map_yaml_file}
+    param_substitutions = {'use_sim_time': use_sim_time, 'yaml_filename': map_yaml_file}
 
-    configured_params = RewrittenYaml(
-        source_file=params_file,
-        root_key=namespace,
-        param_rewrites=param_substitutions,
+    configured_params = RewrittenYaml(source_file=params_file, root_key=namespace, param_rewrites=param_substitutions,
         convert_types=True)
 
-    return LaunchDescription([
-        # Set env var to print messages to stdout immediately
+    return LaunchDescription([# Set env var to print messages to stdout immediately
         SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
-        DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
+        DeclareLaunchArgument('namespace', default_value='', description='Top-level namespace'),
 
-        DeclareLaunchArgument(
-            'map',
-            default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
+        DeclareLaunchArgument('map', default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
             description='Full path to map yaml file to load'),
 
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
+        DeclareLaunchArgument('use_sim_time', default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
-        DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
+        DeclareLaunchArgument('autostart', default_value='true', description='Automatically startup the nav2 stack'),
 
-        DeclareLaunchArgument(
-            'params_file',
-            default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
+        DeclareLaunchArgument('params_file', default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
             description='Full path to the ROS2 parameters file to use'),
 
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[configured_params],
+        Node(package='nav2_map_server', executable='map_server', name='map_server', output='screen',
+            parameters=[configured_params], remappings=remappings),
+
+        Node(package='nav2_amcl', executable='amcl', name='amcl', output='screen', parameters=[configured_params],
             remappings=remappings),
 
-        Node(
-            package='nav2_amcl',
-            executable='amcl',
-            name='amcl',
+        Node(package='nav2_lifecycle_manager', executable='lifecycle_manager', name='lifecycle_manager_localization',
             output='screen',
-            parameters=[configured_params],
-            remappings=remappings),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_localization',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
-                        {'node_names': lifecycle_nodes}])
-    ])
+            parameters=[{'use_sim_time': use_sim_time}, {'autostart': autostart}, {'node_names': lifecycle_nodes}])])
