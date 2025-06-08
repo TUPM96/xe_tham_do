d7a0bb726c02ddb138746fa1b45ff3c7be87740d
TuPM <tupm.citgroup@gmail.com>
2025-06-08 15:19:22 +0700

    add

--- ros2_ws/xe_tham_do/launch/launch_robot.launch.py
+++ ros2_ws/xe_tham_do/launch/launch_robot.launch.py
@@ -10,8 +10,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name = 'xe_tham_do'
 
@@ -42,22 +40,6 @@ def generate_launch_description():
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(target_action=controller_manager, on_start=[joint_broad_spawner], ))
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
     # Launch them all!
-    return LaunchDescription([rsp, # joystick,
+    return LaunchDescription([rsp,
         twist_mux, delayed_controller_manager, delayed_diff_drive_spawner, delayed_joint_broad_spawner])
