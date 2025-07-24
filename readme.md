c499609a962bad451f47d32a070de318c72a0ce6
TuPM <tupm.citgroup@gmail.com>
2025-07-25 02:55:37 +0700

    add

--- readme.md
+++ readme.md
@@ -49,7 +49,7 @@ docker-compose up -d
 
 # Chỉ chạy khi muốn build lại từ đầu
 # colcon build --symlink-install        
-
+# colcon build --packages-select xe_tham_do
 ```
 **Sau đó có thể chạy lệnh bên dưới**
 
@@ -119,7 +119,7 @@ source /opt/ros/humble/install/setup.bash
 
 source install/setup.bash
 
-ros2 launch xe_tham_do rplidar.launch.py serial_port:=/dev/serial/by-path/platform-70090000.xusb-usb-0:2.2:1.0-port0
+ros2 launch xe_tham_do rplidar.launch.py serial_port:=/dev/ttyUSB0
 ```
 
 * ###  Chạy slam toolbox để tạo map
--- ros2_ws/src/xe_tham_do/launch/rplidar.launch.py
+++ ros2_ws/src/xe_tham_do/launch/rplidar.launch.py
@@ -4,16 +4,28 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+def resolve_serial_port(port_path):
+    # Nếu là symlink by-path thì chuyển sang ttyUSBx
+    if os.path.exists(port_path) and os.path.islink(port_path):
+        return os.path.realpath(port_path)
+    return port_path
 
 def generate_launch_description():
 
     serial_port_arg = DeclareLaunchArgument('serial_port', default_value='/dev/ttyUSB0',
-        description='Serial port to which the RPLIDAR is connected')
+                                            description='Serial port to which the RPLIDAR is connected')
 
-    return LaunchDescription([# Declare the argument
+    serial_port = LaunchConfiguration('serial_port')
+
+    return LaunchDescription([
         serial_port_arg,
-
-        # Node configuration
-        Node(package='rplidar_ros', executable='rplidar_composition', output='screen', parameters=[
-            {'serial_port': LaunchConfiguration('serial_port'), 'serial_baudrate': 115200, 'frame_id': 'laser_frame',
-                'angle_compensate': True, 'scan_mode': 'Standard'}])])
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[
+                {'serial_port': serial_port, 'serial_baudrate': 115200, 'frame_id': 'laser_frame',
+                 'angle_compensate': True, 'scan_mode': 'Standard'}
+            ]
+        )
+    ])
