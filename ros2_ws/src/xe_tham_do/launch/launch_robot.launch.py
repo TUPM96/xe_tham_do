27063e75c7302229fa751e32c00a99f6d7ecbacc
TuPM <tupm.citgroup@gmail.com>
2025-08-04 23:41:00 +0700

    add

--- ros2_ws/src/xe_tham_do/launch/launch_robot.launch.py
+++ ros2_ws/src/xe_tham_do/launch/launch_robot.launch.py
@@ -7,68 +7,39 @@ from launch.event_handlers import OnProcessStart
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+
 
 def generate_launch_description():
+
     package_name = 'xe_tham_do'
 
-    rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]
-        ),
-        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
-    )
+    rsp = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
+        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items())
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
-    twist_mux = Node(
-        package="twist_mux",
-        executable="twist_mux",
-        parameters=[twist_mux_params],
-        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
-    )
+    twist_mux = Node(package="twist_mux", executable="twist_mux", parameters=[twist_mux_params],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')])
 
-    # Đường dẫn tới file xacro/urdf của bạn!
-    urdf_path = os.path.join(get_package_share_directory(package_name), 'description', 'robot.urdf.xacro')
-    robot_description = ParameterValue(
-        Command(['xacro', urdf_path]),
-        value_type=str
-    )
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description}, controller_params_file]
-    )
+    controller_manager = Node(package="controller_manager", executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description}, controller_params_file])
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_cont"],
-    )
+    diff_drive_spawner = Node(package="controller_manager", executable="spawner", arguments=["diff_cont"], )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(target_action=controller_manager, on_start=[diff_drive_spawner])
-    )
+        event_handler=OnProcessStart(target_action=controller_manager, on_start=[diff_drive_spawner], ))
 
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_broad"],
-    )
+    joint_broad_spawner = Node(package="controller_manager", executable="spawner", arguments=["joint_broad"], )
 
     delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(target_action=controller_manager, on_start=[joint_broad_spawner])
-    )
+        event_handler=OnProcessStart(target_action=controller_manager, on_start=[joint_broad_spawner], ))
 
     # Launch them all!
-    return LaunchDescription([
-        rsp,
-        twist_mux,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
-    ])
+    return LaunchDescription([rsp,
+        twist_mux, delayed_controller_manager, delayed_diff_drive_spawner, delayed_joint_broad_spawner])
