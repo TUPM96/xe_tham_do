44161fc2ead84bf8491bdea3c781365f4e5a5bb1
TuPM <tupm.citgroup@gmail.com>
2025-08-04 23:31:11 +0700

    add

--- ros2_ws/src/xe_tham_do/launch/launch_robot.launch.py
+++ ros2_ws/src/xe_tham_do/launch/launch_robot.launch.py
@@ -21,12 +21,18 @@ def generate_launch_description():
     twist_mux = Node(package="twist_mux", executable="twist_mux", parameters=[twist_mux_params],
         remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')])
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    robot_description = ParameterValue(
+        Command(['ros2 param get --hide-type /robot_state_publisher robot_description']),
+        value_type=str
+    )
 
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
-    controller_manager = Node(package="controller_manager", executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description}, controller_params_file])
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description}, controller_params_file]
+    )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
