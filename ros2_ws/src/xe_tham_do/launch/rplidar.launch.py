f145a034aeb015a56b2e3026534b4f098927314b
TuPM <tupm.citgroup@gmail.com>
2025-08-09 00:48:21 +0700

    add

--- ros2_ws/src/xe_tham_do/launch/rplidar.launch.py
+++ ros2_ws/src/xe_tham_do/launch/rplidar.launch.py
@@ -4,28 +4,35 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-def resolve_serial_port(port_path):
-    # Nếu là symlink by-path thì chuyển sang ttyUSBx
-    if os.path.exists(port_path) and os.path.islink(port_path):
-        return os.path.realpath(port_path)
-    return port_path
 
 def generate_launch_description():
+    # Declare arguments
+    serial_port_arg = DeclareLaunchArgument(
+        'serial_port',
+        default_value='/dev/ttyUSB0',
+        description='Serial port to which the RPLIDAR is connected'
+    )
 
-    serial_port_arg = DeclareLaunchArgument('serial_port', default_value='/dev/ttyUSB0',
-                                            description='Serial port to which the RPLIDAR is connected')
-
-    serial_port = LaunchConfiguration('serial_port')
+    scan_mode_arg = DeclareLaunchArgument(
+        'scan_mode',
+        default_value='Sensitivity',
+        description='Scan mode of the RPLIDAR'
+    )
 
     return LaunchDescription([
         serial_port_arg,
+        scan_mode_arg,
+
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
             output='screen',
-            parameters=[
-                {'serial_port': serial_port, 'serial_baudrate': 115200, 'frame_id': 'laser_frame',
-                 'angle_compensate': True, 'scan_mode': 'Standard'}
-            ]
+            parameters=[{
+                'serial_port': LaunchConfiguration('serial_port'),
+                'serial_baudrate': 115200,
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': LaunchConfiguration('scan_mode')
+            }]
         )
     ])
