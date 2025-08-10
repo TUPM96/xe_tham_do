361c518cc2456bda315e3f548633df8f664fe166
TuPM <tupm.citgroup@gmail.com>
2025-08-11 00:49:53 +0700

    add

--- ros2_ws/src/xe_tham_do/launch/localization_launch.py
+++ ros2_ws/src/xe_tham_do/launch/localization_launch.py
@@ -11,7 +11,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('xe_tham_do')
+    bringup_dir = get_package_share_directory('xe_lidar')
 
     namespace = LaunchConfiguration('namespace')
     map_yaml_file = LaunchConfiguration('map')
@@ -20,68 +20,36 @@ def generate_launch_description():
     params_file = LaunchConfiguration('params_file')
     lifecycle_nodes = ['map_server', 'amcl']
 
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'yaml_filename': map_yaml_file}
+    param_substitutions = {'use_sim_time': use_sim_time, 'yaml_filename': map_yaml_file}
 
-    configured_params = RewrittenYaml(
-        source_file=params_file,
-        root_key=namespace,
-        param_rewrites=param_substitutions,
+    configured_params = RewrittenYaml(source_file=params_file, root_key=namespace, param_rewrites=param_substitutions,
         convert_types=True)
 
-    return LaunchDescription([
-        # Set env var to print messages to stdout immediately
+    return LaunchDescription([# Set env var to print messages to stdout immediately
         SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
-        DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
+        DeclareLaunchArgument('namespace', default_value='', description='Top-level namespace'),
 
-        DeclareLaunchArgument(
-            'map',
-            default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
+        DeclareLaunchArgument('map', default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
             description='Full path to map yaml file to load'),
 
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
+        DeclareLaunchArgument('use_sim_time', default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
-        DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
+        DeclareLaunchArgument('autostart', default_value='true', description='Automatically startup the nav2 stack'),
 
-        DeclareLaunchArgument(
-            'params_file',
-            default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
+        DeclareLaunchArgument('params_file', default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
             description='Full path to the ROS2 parameters file to use'),
 
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[configured_params],
+        Node(package='nav2_map_server', executable='map_server', name='map_server', output='screen',
+            parameters=[configured_params], remappings=remappings),
+
+        Node(package='nav2_amcl', executable='amcl', name='amcl', output='screen', parameters=[configured_params],
             remappings=remappings),
 
-        Node(
-            package='nav2_amcl',
-            executable='amcl',
-            name='amcl',
+        Node(package='nav2_lifecycle_manager', executable='lifecycle_manager', name='lifecycle_manager_localization',
             output='screen',
-            parameters=[configured_params],
-            remappings=remappings),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_localization',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
-                        {'node_names': lifecycle_nodes}])
-    ])
+            parameters=[{'use_sim_time': use_sim_time}, {'autostart': autostart}, {'node_names': lifecycle_nodes}])])
